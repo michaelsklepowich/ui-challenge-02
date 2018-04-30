84daeb0b0fbd84ae0ba6d334fdbbcb929f5f6e0f
Michael Sklepowich <michaelsklepowich@Michaels-MacBook-Pro-3.local>
2018-04-29 21:25:18 -0700

    got sprites working with static css file, need to convert to jquery methods

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+$(document).ready(() => {
+
+  $('#unchecked').css({"background-image": "url('imgs/form-elements-sprite.png')", 'background-repeat': "no-repeat -158px -239px"});
+
+
+
+  console.log('not broken');
+   //   ", "no-repeat -158px -239px", "width": "161px",
+   // "height": "160px");
+ });
+
+console.log('test');
